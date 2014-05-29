ca11a8a0e3a8cc5b2d3a2aa191cdcf9a099bcdc0
andre-nunes <andre.garcia.nunes@gmail.com>
2014-05-29 22:32:27 +0100

    Remove examples

--- src/examples/avgAge.groovy
+++
@@ -1,9 +0,0 @@
-//computes the average age from default graph objects
-g = TinkerGraphFactory.createTinkerGraph() //default graph
-total = 0
-for (i in 1..g.vertices.size())
-    if(g.v(i).age != null)
-        total = total + g.v(i).age
-
-avg = total / g.vertices.size()
-println avg 
--- src/examples/helloWorld.groovy
+++
@@ -1,5 +0,0 @@
-//prints hello world 5 times
-a = 'hello'
-b = 'world'
-for (i in 1..5)
-    println "${a} ${b}" 
