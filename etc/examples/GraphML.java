df9e000eb9a3b3ff23f0d794477b28b48cc53b41
andre-nunes <andre.garcia.nunes@gmail.com>
2014-06-05 22:33:40 +0100

    Add example using blueprints' GraphML Writer library

--- etc/examples/GraphML.java
+++ etc/examples/GraphML.java
@@ -2,6 +2,7 @@ package examples;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -9,6 +10,7 @@ import com.tinkerpop.blueprints.Graph;
 import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.blueprints.impls.tg.TinkerGraph;
 import com.tinkerpop.blueprints.util.io.graphml.GraphMLReader;
+import com.tinkerpop.blueprints.util.io.graphml.GraphMLWriter;
 
 // This example reads GraphML data into a graph and prints some of the info contained in the graph
 public class GraphML {
@@ -25,5 +27,18 @@ public class GraphML {
 						+ " has age: " + age);
 			}
 		}
+
+		// Now try exporting the graph into a different file
+		File outputFile = new File("etc/examples/output.graphml");
+		FileOutputStream graphMLOutputStream = new FileOutputStream(outputFile);
+		GraphMLWriter.outputGraph(inputGraph, graphMLOutputStream);
+
+		// Now try normalizing the graph output
+		outputFile = new File("etc/examples/normalizedOutput.graphml");
+		graphMLOutputStream = new FileOutputStream(outputFile);
+		GraphMLWriter writer = new GraphMLWriter(inputGraph);
+		writer.setNormalize(true);
+		writer.outputGraph(graphMLOutputStream);
+
 	}
 }
