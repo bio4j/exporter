89719fa24f573653054838265ce5cbec45103234
andre-nunes <andre.garcia.nunes@gmail.com>
2014-06-09 18:24:16 +0100

    Add class to hold implementation of User Defined Steps

---
+++ src/main/java/Steps.java
@@ -0,0 +1,36 @@
+package main.java;
+
+public class Steps {
+	public void load() {
+		defineGeneOntologyAssoc();
+		defineInteproAssoc();
+		defineReactomeAssoc();
+		defineOrganism();
+		defineEnzymeAssoc();
+		definePfamAssoc();
+	}
+
+	private void defineGeneOntologyAssoc() {
+
+	}
+
+	private void defineInteproAssoc() {
+
+	}
+
+	private void defineReactomeAssoc() {
+
+	}
+
+	private void defineOrganism() {
+
+	}
+
+	private void defineEnzymeAssoc() {
+
+	}
+
+	private void definePfamAssoc() {
+
+	}
+}
