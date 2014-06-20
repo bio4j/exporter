23cb0771f35f9f7625fb417b361b2323b87246b2
andre-nunes <andre.garcia.nunes@gmail.com>
2014-06-21 00:47:21 +0100

    Refactor GeneOntology user-defined steps

--- src/main/java/com/bio4j/exporter/Steps.java
+++ src/main/java/com/bio4j/exporter/Steps.java
@@ -2,6 +2,7 @@ package com.bio4j.exporter;
 
 import java.util.List;
 
+import com.tinkerpop.blueprints.Edge;
 import com.tinkerpop.blueprints.Graph;
 import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.gremlin.java.GremlinPipeline;
@@ -13,7 +14,7 @@ public class Steps {
 	 * @param relation
 	 * @return
 	 */
-	public static List GeneOntologyAssociations(Graph g, String relation) {
+	public static List geneOntologyAssociationsVertices(Graph g, String relation) {
 		List list = null;
 
 		GremlinPipeline pipe = new GremlinPipeline<Vertex, Vertex>(g).V().outE(
@@ -28,11 +29,41 @@ public class Steps {
 	 * @param relation
 	 * @return
 	 */
-	public static List GeneOntologyAssociations(Graph g, int index,
+	public static List geneOntologyAssociationsVertices(Graph g, int index,
 			String relation) {
 		List list = null;
 
 		GremlinPipeline pipe = new GremlinPipeline<Vertex, Vertex>(
+				g.getVertex(index)).outE(relation);
+		list = pipe.toList();
+		return list;
+	}
+
+	/**
+	 * @param g
+	 * @param relation
+	 * @return
+	 */
+	public static List geneOntologyAssociationsEdges(Graph g, String relation) {
+		List list = null;
+
+		GremlinPipeline pipe = new GremlinPipeline<Vertex, Edge>(g).V().outE(
+				relation);
+		list = pipe.toList();
+		return list;
+	}
+
+	/**
+	 * @param g
+	 * @param index
+	 * @param relation
+	 * @return
+	 */
+	public static List geneOntologyAssociationsEdges(Graph g, int index,
+			String relation) {
+		List list = null;
+
+		GremlinPipeline pipe = new GremlinPipeline<Vertex, Edge>(
 				g.getVertex(index)).outE(relation);
 		list = pipe.toList();
 		return list;
