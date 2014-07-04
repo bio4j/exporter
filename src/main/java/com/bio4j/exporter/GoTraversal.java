4533e76d7f26f078f276deecedc14d6bfea80802
andre-nunes <andre.garcia.nunes@gmail.com>
2014-07-04 16:52:02 +0100

    Fix return values

--- src/main/java/com/bio4j/exporter/GoTraversal.java
+++ src/main/java/com/bio4j/exporter/GoTraversal.java
@@ -12,13 +12,13 @@ import com.tinkerpop.gremlin.structure.Vertex;
 public interface GoTraversal<S, E> extends Traversal<S, E> {
 	// Iterates all GeneOntology terms
 	public default GoTraversal<S, Vertex> goTerms() {
-		return (GoTraversal) this.addStep(
+		return (GoTraversal<S, Vertex>) this.addStep(
 				new StartStep<Vertex>(this, this.memory().<Graph>get("g").V()));
 	}
 
 	// Iterates a specific GeneOntology term
 	public default GoTraversal<S, Vertex> goTerm(int id) {
-		return (GoTraversal) this.addStep(
+		return (GoTraversal<S, Vertex>) this.addStep(
 				new StartStep<Vertex>(this, this.memory().<Graph>get("g").v(id)));
 	}
 
@@ -26,20 +26,20 @@ public interface GoTraversal<S, E> extends Traversal<S, E> {
 	public default GoTraversal<S, Vertex> relatedVertices(String rel) {
 		FlatMapStep<Vertex, Vertex> flatMapStep = new FlatMapStep<>(this);
 		flatMapStep.setFunction(v -> v.get().out(rel));
-		return (GoTraversal) this.addStep(flatMapStep);
+		return (GoTraversal<S, Vertex>) this.addStep(flatMapStep);
 	}
 
 	//Iterates all related edges with given relation
 	public default GoTraversal<S, Edge> relatedEdges(String rel) {
 		FlatMapStep<Vertex, Edge> flatMapStep = new FlatMapStep<>(this);
 		flatMapStep.setFunction(v -> v.get().outE(rel));
-		return (GoTraversal) this.addStep(flatMapStep);
+		return (GoTraversal<S, Edge>) this.addStep(flatMapStep);
 	}
 
 	public default GoTraversal<S,String> name() {
 		MapStep<Vertex,String> mapStep = new MapStep<>(this);
 		mapStep.setFunction(v -> v.get().<String>value("com.bio4j.titan.model.go.nodes.TitanGoTerm.TitanGoTermType.name"));
-		return (GoTraversal) this.addStep(mapStep);
+		return (GoTraversal<S,String>) this.addStep(mapStep);
 	}
 
 	public static GoTraversal of() {
