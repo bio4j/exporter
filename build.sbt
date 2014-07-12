e37d7ad8fe1fcf5ff84b81f09f4925e9ef5e9855
andre-nunes <andre.garcia.nunes@gmail.com>
2014-07-12 02:24:27 +0100

    Add plugin class and dependencies

--- build.sbt
+++ build.sbt
@@ -13,9 +13,12 @@ javaVersion := "1.8"
 resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
 
 libraryDependencies ++= Seq(
-  "com.tinkerpop" % "tinkergraph-gremlin" % "3.0.0-SNAPSHOT",
+  "com.tinkerpop" % "tinkerpop" % "3.0.0-SNAPSHOT",
+  "com.tinkerpop" % "gremlin-core" % "3.0.0-SNAPSHOT",
+  "com.tinkerpop" % "gremlin-groovy" % "3.0.0-SNAPSHOT",
+  "com.tinkerpop" % "gremlin-test" % "3.0.0-SNAPSHOT",  
   "commons-cli" % "commons-cli" % "1.2",
-  "junit" % "junit" % "4.11"
+  "junit" % "junit" % "4.11",
+  "org.codehaus.jettison" % "jettison" % "1.3.3"
 )
-
 
---
+++ src/main/java/com/bio4j/exporter/Bio4jGremlinPlugin.java
@@ -0,0 +1,27 @@
+package com.bio4j.exporter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.tinkerpop.gremlin.groovy.plugin.GremlinPlugin;
+import com.tinkerpop.gremlin.groovy.plugin.PluginAcceptor;
+
+public class Bio4jGremlinPlugin implements GremlinPlugin {
+	
+	private static final String IMPORT = "import ";
+    private static final String DOT_STAR = ".*";
+
+    private static final Set<String> IMPORTS = new HashSet<String>() {{
+        add(IMPORT + Bio4jExporter.class.getPackage().getName() + DOT_STAR);
+    }};
+
+    @Override
+    public String getName() {
+        return "bio4j";
+    }
+
+    @Override
+    public void pluginTo(final PluginAcceptor pluginAcceptor) {
+        pluginAcceptor.addImports(IMPORTS);
+    }
+}
